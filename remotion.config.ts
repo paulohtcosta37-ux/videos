d42521632287fea8f826b3f26757fbdf67451228
Paulo <paulo@example.com>
2026-06-28 11:48:10 -0300

    Completely remove unused Tailwind CSS configuration to resolve native binding issues in CI

--- remotion.config.ts
+++ remotion.config.ts
@@ -4,9 +4,7 @@
 // Note: When using the Node.JS APIs, the config file doesn't apply. Instead, pass options directly to the APIs
 
 import { Config } from "@remotion/cli/config";
-import { enableTailwind } from '@remotion/tailwind-v4';
 
 Config.setVideoImageFormat("jpeg");
 Config.setOverwriteOutput(true);
 Config.setChromiumOpenGlRenderer("angle");
-Config.overrideWebpackConfig(enableTailwind);
